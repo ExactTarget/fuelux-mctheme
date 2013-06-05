bb7ef16e348eb7e819b25130d7ece1e5980da7e0
Christopher McCulloh <cmcculloh@gmail.com>
2013-06-04 22:57:48 -0400

    (2.3.0-wip) removing inadvertant library updates etc

--- util/update-fuelux.sh
+++ util/update-fuelux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "running volo add -nostamp -f ExactTarget/fuelux/>2.3"
+echo "running volo add -nostamp -f ExactTarget/fuelux/>2.2"
 echo "(if you want a version of fuelux later than 2.x you will need to update this command!)"
-volo add -nostamp -f ExactTarget/fuelux/>2.3
+volo add -nostamp -f ExactTarget/fuelux/>2.2
 echo "command presumably executed correctly, continuing..."
 echo
