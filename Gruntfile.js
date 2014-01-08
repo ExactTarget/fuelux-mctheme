f6f64cbe801c975e1dce71f06161ae69f9a807a7
kevinparkerson <kparkerson@exacttarget.com>
2014-01-08 18:12:14 -0500

    additional minor gruntfile tweaks. everything should be good to go

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function (grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		clean: {
-			dist: ['dist/build.txt', 'dist/fuelux.zip'],
+			dist: ['dist/**'],
 			zipsrc: ['dist/fuelux']
 		},
 		compress: {
@@ -130,9 +130,9 @@ module.exports = function (grunt) {
 			options: {
 				banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
 					'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-					'<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+					'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
 					'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-					' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+					' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n'
 			},
 			all: {
 				files: {
