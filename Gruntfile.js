b32d6eaaf536f4bbb63119c6b1a1e2b46e558bf2
kevinparkerson <kparkerson@exacttarget.com>
2014-01-08 17:21:47 -0500

    adding grunt-contrib-uglify plugin. specifying plugins in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,15 @@
 /*global module:false, process:false*/
 module.exports = function (grunt) {
 
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-recess');
+
 	// Project configuration.
 	grunt.initConfig({
 	});
