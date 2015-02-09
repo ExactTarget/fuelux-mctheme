c69b33fef9d0849028c5ba6a0adf01c0c29e7b16
Chris Corwin <CCorwin@exacttarget.com>
2015-02-09 13:50:09 -0500

    (GH154---restructure-less-build-to-allow-ampersand-to-not-have-fuelux-in-parents) restructured the less build to initially build the fuelux stuff without the .fuelux wrapper. it gets tacked onto the rules after the fact.

--- Gruntfile.js
+++ Gruntfile.js
@@ -349,9 +349,8 @@ module.exports = function (grunt) {
 		var options = {
 			force: true
 		};
-		grunt.file.delete('less/fuelux-mctheme-no-namespace.less', options);
+		grunt.file.delete('less/fuelux-override-no-namespace.less', options);
 	});
-
 
 
 	// CSS distribution task
