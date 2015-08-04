3b1d968e0eb233f7cb56a78cffc778f6f9c85b26
Christopher McCulloh <cmcculloh@exacttarget.com>
2015-08-04 16:09:12 -0400

    (documentation-tweaks) quick and dirty basic navigation and TOC

--- examples/bootstrap-css/main.js
+++ examples/bootstrap-css/main.js
@@ -1,9 +1,12 @@
 define(function (require) {
 	require('bootstrap');
+	require('jqtoc');
 
 	$('#inputErrorTooltip1, #inputErrorTooltip2').tooltip({
 		title: 'Required Field',
 		placement: 'right',
 		trigger: 'focus'
 	}).tooltip('show');
+
+	$('#toc').toc({'selectors': 'h1,h2'});
 });
