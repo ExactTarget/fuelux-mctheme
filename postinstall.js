56b2896b64eeee7364203ebd0998f2400f2d089d
Stephen James <llama@interactivellama.com>
2016-04-14 16:50:32 -0400

    Use machine user oauth

--- postinstall.js
+++ postinstall.js
@@ -3,3 +3,17 @@ var npm = require('npm');
 if (process.env.INSTALL_FUELUX_MCTHEME_BOWER === 'true') {
 	 require("bower").commands.install();
 }
+
+if (process.env.INSTALL_FUELUX_MINIMAL_LIGHTNING === 'true' && process.env.MACHINE_USER_OAUTH) {
+	npm.load(function(err) {
+		// install module ffi
+		npm.commands.install(['git+https://' + process.env.MACHINE_USER_OAUTH + ':x-oauth-basic@github.com/exacttarget/fuelux-minimal-lightning.git'], function(er, data) {
+
+		});
+
+		npm.on('log', function(message) {
+			// log installation progress
+			console.log(message);
+		});
+	});
+}
