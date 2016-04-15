710905e2ebad84fbbbaca20dd388d87727fe1eb2
Stephen James <llama@interactivellama.com>
2016-04-15 17:16:52 -0400

    Move require NPM into conditional

--- postinstall.js
+++ postinstall.js
@@ -1,10 +1,10 @@
-var npm = require('npm');
-
 if (process.env.INSTALL_FUELUX_MCTHEME_BOWER === 'true') {
 	 require("bower").commands.install();
 }
 
 if (process.env.INSTALL_FUELUX_MINIMAL_LIGHTNING === 'true' && process.env.MACHINE_USER_OAUTH) {
+	var npm = require('npm');
+
 	npm.load(function(err) {
 		// install module ffi
 		npm.commands.install(['git+https://' + process.env.MACHINE_USER_OAUTH + ':x-oauth-basic@github.com/exacttarget/fuelux-minimal-lightning.git'], function(er, data) {
